9f55c574180d44abc2ee990fa020540f4cc04fa2
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-03 06:29:52 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,7 +7,12 @@ class GetRequester
   
   def get_response_body
   uri = URI.parse(URL)
-  response = Net::HTTP.get_response(uri)
+  response = Net::HTTP.get_response_body(uri)
   response.body
 end 
+  def parse_jon
+    examples = JSON.parse(self.get_response_body)
+    examples.collect do |example|
+    example
+end
 end 
