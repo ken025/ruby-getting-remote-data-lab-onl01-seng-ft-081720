de1f53c6415d107d54e304c198a483ca2e2f8140
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-03 06:49:11 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,20 +4,17 @@ require 'json'
 
 class GetRequester
   
-  def initialize
- get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
-get_requester.parse_json
-end 
-  
+  def initialize(url)
+    @url = url
+  end
+
   def get_response_body
-  uri = URI.parse(URL)
-  response = Net::HTTP.get_response_body(uri)
-  response.body
-end 
-  def parse_jon
-    examples = JSON.parse(self.get_response_body)
-    examples.collect do |example|
-    example
-  end 
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
 end
-end 
