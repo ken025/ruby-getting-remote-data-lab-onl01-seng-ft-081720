ab3422316e056cbaee15f896dab78e954a8cb2e2
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-03 06:32:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,7 +3,10 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
-  URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+  def initialize(get_requester)
+ get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.parse_json
+end 
   
   def get_response_body
   uri = URI.parse(URL)
